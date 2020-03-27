dc20ff1e353d48d5a6a5f4a3a471d00de51a5cd1
Damion <damion.lance@gmail.com>
2020-03-27 12:03:39 -0400

    fixed code not being committed

--- src/main/java/Resource.java
+++ src/main/java/Resource.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
 public class Resource {
+    String name;
+    double amount;
+
+    public Resource(String name, double amount){
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addAmount(double amount) {
+        this.amount += amount;
+    }
 }
--- src/test/java/baseClassesTest.java
+++ src/test/java/baseClassesTest.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
 
 public class baseClassesTest {
+
+    @Test
+    void SpaceStationTest(){
+        SpaceStation myStation = new SpaceStation();
+        User myUser = new User("123");
+        Resource cashMoney = new Resource("Cash Money", 100.15);
+
+        myStation.addUser(myUser);
+
+        Assert.assertTrue(myStation.getUsers().contains(myUser));
+
+        myStation.addResource(cashMoney);
+
+        Assert.assertTrue(myStation.getResources().size() == 1);
+        Assert.assertTrue(myStation.getResources().contains(cashMoney));
+
+        myStation.addResource("Cash Money", 50);
+
+        Assert.assertTrue(myStation.getResources().get(0).amount == 150.15);
+
+        myStation.addResource(cashMoney);
+
+        Assert.assertFalse(myStation.getResources().size() == 2);
+    }
+
+    @Test
+    void ResourceTest(){
+        Resource myResource = new Resource("resource", 10);
+        myResource.addAmount(10);
+
+        Assert.assertTrue(myResource.getAmount() == 20);
+
+        myResource.setAmount(50);
+
+        Assert.assertTrue(myResource.getAmount() == 50);
+    }
+
 }
