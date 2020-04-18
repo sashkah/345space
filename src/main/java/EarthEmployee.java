93f599a358c06f009ce550c46ba0e3ae484bf54f
KandacePC <krichards1@ithaca.edu>
2020-04-18 15:41:27 -0400

    ade EarthEmployee class

---
+++ src/main/java/EarthEmployee.java
@@ -0,0 +1,23 @@
+public class EarthEmployee {
+
+    public String employeeId;
+
+    public EarthEmployee(String employeeIdIn){
+        this.employeeId=employeeIdIn;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+    public void blockResource(String resourceName){
+
+    }
+    public void checkResources(){
+
+    }
+
+}
